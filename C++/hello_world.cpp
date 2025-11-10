d29e3ced44587518f09629d717b4d575dcba058e
nereus_coding <nereuschen0122@gmail.com>
2025-11-10 21:15:49 +0800

    New files

---
+++ C++/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Include the iostream library for input and output
+using namespace std; // Use the standard namespace
+
+int main()
+{
+    cout << "Hello, World!" << endl; // endl outputs a newline character(換行符號) after the message
+    // cout is the standard output stream
+    return 0; // Indicate(表示) that the program ended successfully
+}
