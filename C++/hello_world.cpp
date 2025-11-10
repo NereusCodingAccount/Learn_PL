cf60fa383bbd154b2258de2842847ec9b48e9cc0
nereus_coding <nereuschen0122@gmail.com>
2025-11-10 21:18:06 +0800

    New content

--- C++/hello_world.cpp
+++ C++/hello_world.cpp
@@ -1,3 +1,5 @@
+// hello_world.cpp
+// A simple C++ program that prints "Hello, World!" to the console
 #include <iostream> // Include the iostream library for input and output
 using namespace std; // Use the standard namespace
 
